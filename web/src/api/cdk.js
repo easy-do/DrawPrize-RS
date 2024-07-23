7be5575f6242ee97d71acc4fb75ea62b1d0c44ab
gebilaoyu <yuzhanfeng@yunlizhihui.com>
2024-07-23 19:45:12 +0800

    fix ESLint error

--- web/src/api/cdk.js
+++ web/src/api/cdk.js
@@ -7,5 +7,5 @@ export const getCdkInfo = (id) => get("/api/cdk/info/"+id);
 export const addCdk = (param) => postRequestBody("/api/cdk/add",param);
 export const updateCdk = (param) => postRequestBody("/api/cdk/update",param);
 export const exportCdk = (param) => postRequestBody("/api/cdk/export",param);
-export const useCdk = (param) => postRequestBody("/api/cdk/use_cdk",param);
+export const requetUseCdk = (param) => postRequestBody("/api/cdk/use_cdk",param);
 
